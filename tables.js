d10e14cde77ab452f01e4e7865040e89e2b16b55
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-08 18:40:53 -0700

    column function

--- tables.js
+++ tables.js
@@ -1,17 +1,29 @@
 
 
-const id = {
-    field: 'id', type: 'INTEGER', null: 'NOT NULL', key: 'PRIMARY KEY', default: null, extra: 'AUTO_INCREMENT'
-}
+const column = (fieldName, type, nullability, key, defaultVal, extra) => {
+    const columnVals =
+    {
+        field: fieldName,
+        type: type,
+        null: nullability,
+        key: key,
+        default: defaultVal,
+        extra: extra
+    };
+    return columnVals;
+};
+
+
+const id = column('id', 'INTEGER', 'NOT NULL', 'PRIMARY KEY', null, 'AUTO_INCREMENT')
+
 
 const tables = [
     {
         tableName: 'users',
         columns: [
             id,
-            { field: 'first_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
-            { field: 'last_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
-            { field: 'birth_date', type: 'DATE', null: 'NOT NULL', key: null, default: null, extra: null },
+            { field: 'username', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
+            { field: 'password', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
         ]
     },
     {
