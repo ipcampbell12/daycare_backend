3042c480913346b3db102b6f181248df77d030e8
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-08 18:21:07 -0700

    create table functions

--- create_db.js
+++ create_db.js
@@ -7,8 +7,25 @@ const connection = mysql.createConnection({
     database: 'join_us'
 });
 
+const createDatabase = (dbName) => {
+    const query = `CREATE DATABASE ${dbName}`
+
+    connection.query(query, function (err, result, field) {
+        if (err) throw err;
+        console.log(`${dbName} created!`)
+    });
+
+}
 
 const createTable = (table) => {
+    connection.query(createTableQuery(table), function (err, result, field) {
+        if (err) throw err;
+        console.log(`${table.tableName} table created!`)
+    });
+
+}
+
+const createTableQuery = (table) => {
     const tableName = `CREATE TABLE ${table.tableName} ( \n`
     const columns = table.columns.map(column => {
         return (Object.values(column)).join(' ')
@@ -19,21 +36,17 @@ const createTable = (table) => {
 }
 
 
-const table = {
-    tableName: 'people',
+const children = {
+    tableName: 'children',
     columns: [
         { field: 'id', type: 'INTEGER', null: 'NOT NULL', key: 'PRIMARY KEY', default: null, extra: 'AUTO_INCREMENT' },
         { field: 'first_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
         { field: 'last_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
+        { field: 'birth_date', type: 'DATE', null: 'NOT NULL', key: null, default: null, extra: null },
     ]
 }
 
-
-connection.query(createTable(table), function (err, result, field) {
-    if (err) throw err;
-    console.log(`${table.tableName} table created!`)
-});
-
+createTable(children)
 
 module.exports = {
     connection: connection
