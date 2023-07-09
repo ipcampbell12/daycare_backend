a959b6334f7b1338712e117515e33cff896c33e9
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-08 18:50:12 -0700

    added more tables to table array

--- tables.js
+++ tables.js
@@ -1,6 +1,6 @@
 
 
-const column = (fieldName, type, nullability, key, defaultVal, extra) => {
+const createColumn = (fieldName, type, nullability = 'NOT NULL', key = null, defaultVal = null, extra = null) => {
     const columnVals =
     {
         field: fieldName,
@@ -13,8 +13,7 @@ const column = (fieldName, type, nullability, key, defaultVal, extra) => {
     return columnVals;
 };
 
-
-const id = column('id', 'INTEGER', 'NOT NULL', 'PRIMARY KEY', null, 'AUTO_INCREMENT')
+const id = createColumn('id', 'INTEGER', 'NOT NULL', 'PRIMARY KEY', null, 'AUTO_INCREMENT');
 
 
 const tables = [
@@ -22,21 +21,41 @@ const tables = [
         tableName: 'users',
         columns: [
             id,
-            { field: 'username', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
-            { field: 'password', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
+            createColumn('uswername', 'VARCHAR(255)'),
+            createColumn('password', 'VARCHAR(255)')
         ]
     },
     {
         tableName: 'children',
         columns: [
             id,
-            { field: 'first_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
-            { field: 'last_name', type: 'VARCHAR(255)', null: 'NOT NULL', key: null, default: null, extra: null },
-            { field: 'birth_date', type: 'DATE', null: 'NOT NULL', key: null, default: null, extra: null },
+            createColumn('first_name', 'VARCHAR(255)'),
+            createColumn('last_name', 'VARCHAR(255)'),
+            createColumn('date_of_birth', 'DATE'),
         ]
     },
     {
+        tableName: 'parents',
+        columns: [
+            id,
+            createColumn('first_name', 'VARCHAR(255)'),
+            createColumn('last_name', 'VARCHAR(255)'),
+            createColumn('email', 'VARCHAR(255)'),
+            createColumn('phone_number', 'CHAR(12)'),
+            createColumn('spouse_id', 'INTEGER', 'FOREIGN KEY'),
+            createColumn('child_id', 'INTEGER', 'FOREIGN KEY'),
+        ]
 
-    }
+    },
+    {
+        tableName: 'children_parents',
+        columns: [
+            id,
+            createColumn('first_name', 'VARCHAR(255)'),
+            createColumn('child_id', 'INTEGER', 'FOREIGN KEY'),
+            createColumn('parents_id', 'INTEGER', 'FOREIGN KEY'),
+        ]
+
+    },
 
 ]
