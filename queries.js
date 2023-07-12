69637f6586ff2d640becdec0b991804700eb73e3
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-12 15:18:38 -0700

    executeQuery function

--- queries.js
+++ queries.js
@@ -1,4 +1,14 @@
 const { connection } = require('./create_db');
 
+//crud
+
+
+const executeQuery = (query, response, values = null) => {
+    connection.query(query, [values], function (err, result) {
+        if (err) throw err;
+        console.log('Connected do database!')
+        response.send(result)
+    });
+}
 
 
