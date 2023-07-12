36eef617a8fb099fe5f6ff714c9f8d089ae38aae
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-12 15:28:03 -0700

    base CRUD queries

--- queries.js
+++ queries.js
@@ -9,6 +9,40 @@ const executeQuery = (query, response, values = null) => {
         console.log('Connected do database!')
         response.send(result)
     });
+};
+
+
+function createRow(value, response, table, columns) {
+    const query = `INSERT INTO ${table} (${columns.joins(',')}) VALUES ?`;
+
+    executeQuery(query, response, [value]);
+
+};
+
+function createMultipleRows(values, response, table, columns) {
+    const query = `INSERT INTO ${table} (${columns.joins(',')}) VALUES (?)`;
+
+    executeQuery(query, response, [values]);
+
+};
+
+function deleteRow(value, table, column, response) {
+    const query = `DELETE FROM ${table} WHERE ${column}=${value}`;
+
+    executeQuery(query, response);
+};
+
+function updateRow(value, table, columnToUpdate, filterColumn, filterValue, response) {
+
+    const query = `UPDATE ${table} SET ${columnToUpdate} = ${value} WHERE  ${filterColumn} =${filterValue}`;
+
+    executeQuery(query, response);
 }
 
 
+module.exports = {
+    createRow: createRow,
+    createMultipleRows: createMultipleRows,
+    deleteRow: deleteRow,
+    updateRow: updateRow
+};
