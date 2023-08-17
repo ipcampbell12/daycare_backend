b60eb1a709728466631ea2902fbd76a262ec4469
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-17 11:57:19 -0700

    moved sql files into separate folder

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-const providers = require("./routes/providers");
-const children = require('./routes/children');
-const visits = require("./routes/visits");
+
 
 const app = express()
 
 app.use(express.json());
 app.use(morgan('tiny'))
 
-app.use('/api/providers', providers.providerRouter)
-app.use('/api/providers', children.childrenRouter)
-app.use('/api/providers', visits.visitsRouter)
+
 
 const port = process.env.PORT || 3003;
 
