c0d2a7f20be686e5efc2bc319256239f05ce715c
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-17 16:29:26 -0700

    parents-children associationt able

--- models/ParentsChildren.js
+++ models/ParentsChildren.js
@@ -1,3 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
+    const ParentsChildren = sequelize.define("ParentsChildren", {
 
-}
+    });
+
+    Parents.belongsToMany(Children, { through: ParentsChildren });
+    Children.belongsToMany(Parents, { through: ParentsChildren });
+
+    return ParentsChildren;
+};
