d0c8c58cd88b350fd53d90648b5284227646215b
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-24 10:49:52 -0700

    added validation to visits table

--- models/visit.js
+++ models/visit.js
@@ -18,11 +18,35 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Visit.init({
-    visitDate: DataTypes.DATE,
-    visitCost: DataTypes.DECIMAL,
-    paidFor: DataTypes.BOOLEAN
+    visitDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Visit must have a date" },
+        notEmpty: { msg: "Date must not be empty" },
+        isDate: { msg: "Must be a valid date" }
+      }
+    },
+    visitCost: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Vist must have cost" },
+        notEmpty: { msg: "Cost must not be empty" },
+        isDecimal: { msg: "Cost must be valid " }
+      }
+    },
+    paidFor: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "visit must have status" },
+        notEmpty: { msg: "Status must not be empty" },
+      }
+    },
   }, {
     sequelize,
+    tableName: "visits",
     modelName: 'Visit',
   });
   return Visit;
