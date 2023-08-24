958323e01fdd63751afcb8c94a86499553fae019
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-24 10:30:54 -0700

    relationship on child parents

--- models/childparent.js
+++ models/childparent.js
@@ -9,8 +9,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Child, Parent }) {
       // define association here
+      this.belongsTo(Child, {
+        foreignKey: 'childId',
+        as: "child"
+      });
+      this.belongsTo(Parent, {
+        foreignKey: 'parentId',
+        as: 'parent'
+      })
     }
   }
   ChildParent.init({
