c9809e3c9e6a964565221de8dbaab16dfd88ef16
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-27 08:43:54 -0700

    added payment routes

--- routes/Payments.js
+++ routes/Payments.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { Payment } = require('../models/payment');
 
+router.post('/', async (req, res) => {
+    try {
+        const payment = req.body;
+        await Child.create(payment);
 
+        return res.send(payment);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+})
+
+router.get('/', async (req, res) => {
+
+    try {
+        const payments = await Payment.findAll();
+
+        return res.send(payments);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
